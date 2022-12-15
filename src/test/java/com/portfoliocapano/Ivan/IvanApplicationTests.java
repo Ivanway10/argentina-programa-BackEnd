0db5979615e97385e69b467f02c04db624e7769f
ivanway10@gmail.com <ivanway10@gmail.com>
2022-12-15 18:45:40 -0300

    Creando estructura backend

---
+++ src/test/java/com/portfoliocapano/Ivan/IvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliocapano.Ivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
