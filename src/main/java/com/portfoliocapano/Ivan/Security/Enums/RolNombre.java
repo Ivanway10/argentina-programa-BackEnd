63a0f26eaedd06c7b17526caabbdc5d0e74efc65
ivan <ivanway10@gmail.com>
2023-03-06 01:18:55 -0300

    Creando paquetes y clases java para que funcione el Login con Jason Web Token

---
+++ src/main/java/com/portfoliocapano/Ivan/Security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfoliocapano.Ivan.Security.Enums;
+
+/**
+ *
+ * @author ivanw
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER   
+}
---
+++ src/main/java/com/portfoliocapano/Ivan/Security/MainSecurity.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfoliocapano.Ivan.Security;
+
+import com.portfoliocapano.Ivan.Security.Service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(preEnabled = true)
+public class MainSecurity {
+    @Autowired
+    UserDetailsImpl userDetailsServiceImpl;
+    
+    
+}
