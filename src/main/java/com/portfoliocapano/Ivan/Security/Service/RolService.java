bb08badd9b805fffd6c3f2359dee8c8e56c1ee34
ivan <ivanway10@gmail.com>
2023-04-03 11:01:52 -0300

    tratando de que postman me deje de tirar error 404

--- src/main/java/com/portfoliocapano/Ivan/Security/Service/RolService.java
+++ src/main/java/com/portfoliocapano/Ivan/Security/Service/RolService.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfoliocapano.Ivan.Security.Service;
 
 import com.portfoliocapano.Ivan.Security.Entity.Rol;
